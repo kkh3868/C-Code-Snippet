173670bd1c44653db682d73a9f43e9b41a1184ce
kkh3868 <108853208+kkh3868@users.noreply.github.com>
2023-12-16 11:33:20 +0900

    Asynchoronous Project
    비동기 프로그래밍에 대한 이해를 위해 간단한 프로그레스바를 비동기적으로 여러개 만들어봤습니다

---
+++ AsynchronousProject/AsynchronousProject/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace AsynchronousProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(89, 102);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(635, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(89, 186);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(635, 23);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(89, 270);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(635, 23);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Work1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Work2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Work3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 60);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Work Start1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(268, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 60);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Work Start2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(447, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 60);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Work Start3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(626, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 60);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Work Start4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ AsynchronousProject/AsynchronousProject/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsynchronousProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.WhenAll(UpdateProgressBarAsync(progressBar1, 100),
+                               UpdateProgressBarAsync(progressBar2, 150),
+                               UpdateProgressBarAsync(progressBar3, 200));
+
+            MessageBox.Show("작업이 완료되었습니다.");
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => this.BeginInvoke(new Action(() => UpdateProgressBarAsync(progressBar1, 100))));
+            await Task.Run(() => this.BeginInvoke(new Action(() => UpdateProgressBarAsync(progressBar2, 150))));
+            await Task.Run(() => this.BeginInvoke(new Action(() => UpdateProgressBarAsync(progressBar3, 200))));
+
+            MessageBox.Show("작업이 완료되었습니다.");
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            await UpdateProgressBarAsync(progressBar1, 100);
+            await UpdateProgressBarAsync(progressBar2, 150);
+            await UpdateProgressBarAsync(progressBar3, 200);
+
+            MessageBox.Show("작업이 완료되었습니다.");
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            // Task를 직접 생성하여 병렬로 실행
+            var task1 = UpdateProgressBarAsync(progressBar1, 100);
+            var task2 = UpdateProgressBarAsync(progressBar2, 150);
+            var task3 = UpdateProgressBarAsync(progressBar3, 200);
+
+            // 모든 Task가 완료될 때까지 대기
+            await Task.WhenAll(task1, task2, task3);
+
+            MessageBox.Show("작업이 완료되었습니다.");
+        }
+
+        private async Task UpdateProgressBarAsync(ProgressBar progressBar, int maxValue)
+        {
+            // 프로그레스바 초기화
+            progressBar.Value = 0;
+            progressBar.Maximum = maxValue;
+
+            // 비동기 작업 시뮬레이션
+            for (int i = 0; i <= maxValue; i++)
+            {
+                // 10밀리초마다 대기하며 프로그레스바 갱신
+                await Task.Delay(10);
+                progressBar.Value = i;
+            }
+        }
+    }
+}
